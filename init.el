39e27f8e486012f5d5a025990528a023121efdaa
unknown <sshaw@.(none)>
2011-09-06 11:02:54 +1000

    Added fsharp mode

--- init.el
+++ init.el
@@ -50,4 +50,9 @@
 ;(require 'feature-mode)
 ;(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
+;;; F# mode
+(setq load-path (cons "~/.emacs.d/fsharp" load-path))
+(setq auto-mode-alist (cons '("\\.fs[iylx]?$" . fsharp-mode) auto-mode-alist))
+(autoload 'fsharp-mode "fsharp" "Major mode for editing F# code." t)
+(autoload 'run-fsharp "inf-fsharp" "Run an inferior F# process." t)
 
