7cf9d76b0822d91c61c38a6e1705925faea6f0b6
Scott Shaw <sshaw@Scotts-MacBook-Air.local>
2011-10-27 09:50:47 +1100

    minor edits

--- init.el
+++ init.el
@@ -56,3 +56,15 @@
 (autoload 'fsharp-mode "fsharp" "Major mode for editing F# code." t)
 (autoload 'run-fsharp "inf-fsharp" "Run an inferior F# process." t)
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(markdown-command "/usr/local/bin/markdown"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
